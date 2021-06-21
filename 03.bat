ded5fcee9a23f5bf312fd00697558ede679146da
Evgeniya_Pehota <zhenya_247@mail.ru>
2021-06-21 10:47:32 +0300

    adding local repo to remote

--- 03.bat
+++ 03.bat
@@ -2,10 +2,9 @@ git status
 git add *
 echo "commit message:"
 git commit -m "adding local repo to remote"
-git pushgithub
 git branch -M master
-git pushgithub
-git push --set-upstreamgithub master
-git  git pushgitlab
+git push github
+git push --set-upstream github master
+git git pushgitlab
 git push bitbucket
 pause
